ee37b4584f0b6bd9867c14633a2a10bec8c0abfb
Tuong Van Nguyen <tvan262@yahoo.com>
2018-05-18 16:09:10 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,11 @@
 # Make your shoe class here!
 
 class Shoe
-  attr_accessor :brand, :color, :size, :material
-  attr_reader :title
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
  
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand = brand
   end
  
   def turn_page
